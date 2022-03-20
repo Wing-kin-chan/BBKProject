9003033bcac770b476bb4a690a06d1fb1ef70a06
Wing-kin Chan <wing_kin_chan@hotmail.com>
2022-03-20 22:36:22 +0000

    +gene table, +coding region table, code to load data

--- createdb/createdb.py
+++ createdb/createdb.py
@@ -105,41 +105,61 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 #Create tables
-createGeneTable = (
-    "DROP TABLE IF EXISTS genes;"
-    "CREATE TABLE genes ("
-    "Accession VARCHAR(12) PRIMARY KEY,"
-    "Date DATE NOT NULL"
-    "Locus VARCHAR(6) NOT NULL,"
-    "GeneID VARCHAR(6) NOT NULL,"
-    "ProteinProduct VARCHAR(6) NOT NULL"
-    "Definition VARCHAR(255) NOT NULL,"
-    "Source VARCHAR(60) NOT NULL,"
-    "Sequence LONGBLOB NOT NULL,"
-    "Translation LONGBLOB NOT NULL,"
-    "CodingRegions BLOB NOT NULL,"
-    "Frame INT(1) NOT NULL"
-    "CodingSequence LONGBLOB NOT NULL"
-    ")ENGINE = INNODB;"
-)
+create_tbls = '''
+DROP TABLE IF EXISTS genes;
+DROP TABLE IF EXISTS coding_regions;
+
+CREATE TABLE genes(
+    Accession VARCHAR(12) PRIMARY KEY,
+    Date DATE NOT NULL,
+    Locus VARCHAR(40) NOT NULL,
+    GeneID VARCHAR(8) NOT NULL,
+    Protein VARCHAR(255) NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    Source VARCHAR(60) NOT NULL,
+    Sequence LONGBLOB NOT NULL,
+    Coding_Seq LONGBLOB NOT NULL,
+    Frame INT(1) NOT NULL,
+    Translation LONGBLOB NOT NULL 
+)ENGINE = InnoDB;
+
+CREATE TABLE coding_regions(
+    Accession VARCHAR(12) PRIMARY KEY,
+    Region_No INT(100) NOT NULL,
+    Range VARCHAR(24) NOT NULL,
+    FOREIGN KEY(Accession)
+        REFERENCES genes(Accession)
+        ON DELETE CASCADE
+)Engine = InnoDB;
+'''
 
 try:
-    cursor.execute(createGeneTable)
-    print(f'Created Empty Gene Table')
-    cursor.close()
+    cursor.execute(create_tbls)
+    print('Successfully created tables')
+    cursor.commit()
 except pymysql.err.Error as e:
-    print(f'Failed creating Gene Table: {e}')
-    cursor.close()
+    print(f'Failed creating tables: {e}')
     exit(1)
     
 #Load data into SQL Server
-load_data = (
-    "INSERT INTO genes ("
-    "Accession, Date, Locus, GeneID, ProteinProduct, Description, Source, Sequence, ReadingFrame, Translation, CodingRegions"
-    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-)
+load_genetbl = '''
+INSERT INTO genes(Accession, Date, Locus, GeneID, Protein, Description, Source, Sequence, Frame, Translation)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+'''
 
-cursor.execute(load_data, (accessions, dates, loci, geneIDs, protein_products, descriptions, sources, sequences, reading_frames, translations))
+load_codingtbl = '''
+INSERT INTO coding_regions VALUES(%s, %s, %s);
+'''
+
+cursor.execute(load_genetbl, (accessions, dates, loci, geneIDs, protein_products, descriptions, sources, sequences, reading_frames, translations))
 cursor.commit()
-cursor.close()
 
+i = -1 #Counter to reference accession numbers for DB Table PK
+for entry in coding_regions:
+    i += 1
+    for k, v in entry.items():
+        cursor.execute(load_codingtbl, (accessions[i], k, v)) #Loads coding regions (keys) and base ranges (values) into table with accession number 
+        cursor.commit()
+
+cursor.close()
+connection.close()
