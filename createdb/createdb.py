2c680afe43d694df2a2db8cd0801d527d15f378b
Wing-kin Chan <wing_kin_chan@hotmail.com>
2022-03-22 21:35:39 +0000

    Connect DB v4

--- createdb/createdb.py
+++ createdb/createdb.py
@@ -90,11 +90,11 @@ import pymysql
 from pymysql import cursors
 
 #Database connection details:
-dbname = 'cw001'
+dbname = 'biodb'
 dbhost = 'pandora'
 port = 3306
-dbuser = 'cw001'
-dbpass = 'trp38ile'
+dbuser = 'biodb_user'
+dbpass = 'biodb_p'
 
 #Connection object
 connection = pymysql.connect(
@@ -109,33 +109,33 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 #Create tables
-create_tbls = '''
-DROP TABLE IF EXISTS coding_regions;
-DROP TABLE IF EXISTS genes;
+create_tbls = (
+'DROP TABLE IF EXISTS coding_regions;'
+'DROP TABLE IF EXISTS genes;'
 
-CREATE TABLE genes(
-    Accession VARCHAR(12) PRIMARY KEY,
-    Date DATE NOT NULL,
-    Locus VARCHAR(40) NOT NULL,
-    GeneID VARCHAR(8) NOT NULL,
-    Protein VARCHAR(255) NOT NULL,
-    Description VARCHAR(255) NOT NULL,
-    Source VARCHAR(60) NOT NULL,
-    Sequence LONGBLOB NOT NULL,
-    Coding_Seq LONGBLOB NOT NULL,
-    Frame INT(1) NOT NULL,
-    Translation LONGBLOB NOT NULL 
-)ENGINE = InnoDB;
+'CREATE TABLE genes('
+    'Accession VARCHAR(12) PRIMARY KEY,'
+    'Date DATE NOT NULL,'
+    'Locus VARCHAR(40) NOT NULL,'
+    'GeneID VARCHAR(8) NOT NULL,'
+    'Protein VARCHAR(255) NOT NULL,'
+    'Description VARCHAR(255) NOT NULL,'
+    'Source VARCHAR(60) NOT NULL,'
+    'Sequence LONGBLOB NOT NULL,'
+    'Coding_Seq LONGBLOB NOT NULL,'
+    'Frame INT(1) NOT NULL,'
+    'Translation LONGBLOB NOT NULL' 
+')ENGINE = InnoDB;'
 
-CREATE TABLE coding_regions(
-    Accession VARCHAR(12) PRIMARY KEY,
-    Region_No INT(100) NOT NULL,
-    Base_No VARCHAR(24) NOT NULL,
-    FOREIGN KEY(Accession)
-        REFERENCES genes(Accession)
-        ON DELETE CASCADE
-)Engine = InnoDB;
-'''
+'CREATE TABLE coding_regions('
+    'Accession VARCHAR(12) PRIMARY KEY,'
+    'Region_No INT(100) NOT NULL,'
+    'Base_No VARCHAR(24) NOT NULL,'
+    'FOREIGN KEY(Accession)'
+        'REFERENCES genes(Accession)'
+        'ON DELETE CASCADE'
+')Engine = InnoDB;'
+)
 
 try:
     cursor.execute(create_tbls)
@@ -146,14 +146,14 @@ except pymysql.err.Error as e:
     exit(1)
     
 #Load data into SQL Server
-load_genetbl = '''
-INSERT INTO genes(Accession, Date, Locus, GeneID, Protein, Description, Source, Sequence, Frame, Translation)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-'''
+load_genetbl = (
+'INSERT INTO genes(Accession, Date, Locus, GeneID, Protein, Description, Source, Sequence, Frame, Translation)'
+    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
+)
 
-load_codingtbl = '''
-INSERT INTO coding_regions VALUES(%s, %s, %s);
-'''
+load_codingtbl = (
+'INSERT INTO coding_regions VALUES(%s, %s, %s);'
+)
 
 cursor.execute(load_genetbl, (accessions, dates, loci, geneIDs, protein_products, descriptions, sources, sequences, reading_frames, translations))
 cursor.commit()
