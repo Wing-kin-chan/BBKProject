0f8fd7c820960a7a7ad6d712b0cadf0fd0f61493
Wing-kin Chan <wing_kin_chan@hotmail.com>
2022-03-23 21:29:51 +0000

    Working search functions in dbApi

--- cgi-biocomp2/db/dbapi_real.py
+++ cgi-biocomp2/db/dbapi_real.py
@@ -27,29 +27,31 @@ cursor = connection.cursor()
 #To save writing repeated SQL queries of similar structure, definied a single function with arguments to query the database.
 #Each search function will parse their unique arguments to this function      
 def search(querytype: str, query: str, resultlen: int):
-    optional_sql = ', Date, Description, Source, Sequence, Coding_seq, Frame, Translation, Region_No, Range'
-    sql = [
-        'SELECT Accession, GeneID, Protein, Locus',
-        'FROM genes g',
-        'JOIN coding_regions c ON(g.Accession = c.Accession)',
-        'WHERE {} LIKE %{}%'.format(querytype, query),
-        'LIMIT {}'.format(resultlen)
-    ]
-    #List all entries in database
+    cursor = connection.cursor()
+    sql_selectall = 'SELECT * '
+    sql_selectsummary = 'SELECT Accession, GeneID, Product, Locus '
+    sql_location = 'FROM genes g '
+    sql_join = 'JOIN coding_regions c ON(g.Accession = c.Accession) '
+    sql_where = '''WHERE g.{} LIKE '%{}%' '''.format(querytype, query)
+    sql_limit = 'LIMIT {};'.format(resultlen)
+    
+    #List all entry summaries in database
     if querytype == 'getAll' and query == 'getAll':
+        sql = sql_selectsummary + sql_location + ';'
         results = list()
         
-        cursor.execute(sql[0:1])
+        cursor.execute(sql)
         for row in cursor.fetchall():
             results_row = dict()
             results_row['Accession'] = row[0]
             results_row['GeneID'] = row[1]
-            results_row['Protein'] = row[2]
+            results_row['Product'] = row[2]
             results_row['Locus'] = row[3]
             results.append(results_row)
     
     #Return list of entry summaries for search results
-    if querytype in ['Accession', 'GeneID', 'Protein', 'Locus'] and resultlen > 1:
+    if querytype in ['Accession', 'GeneID', 'Product', 'Locus'] and resultlen > 1:
+        sql = sql_selectsummary + sql_location + sql_where + sql_limit
         results = list()
         
         cursor.execute(sql)
@@ -57,40 +59,37 @@ def search(querytype: str, query: str, resultlen: int):
             results_row = dict()
             results_row['Accession'] = row[0]
             results_row['GeneID'] = row[1]
-            results_row['Protein'] = row[2]
+            results_row['Product'] = row[2]
             results_row['Locus'] = row[3]
             results.append(results_row)
     
     #Return all information on specific entry for gene page
-    if querytype in ['Accession', 'GeneID', 'Protein', 'Locus'] and resultlen == 1:
+    if querytype in ['Accession', 'GeneID', 'Product', 'Locus'] and resultlen == 1:
+        sql = sql_selectall + sql_location + sql_join + sql_where + ';'
         results = dict()
         results['Coding Regions'] = dict()
-        sql[0] += optional_sql[1]
         
         cursor.execute(sql)
         for row in cursor.fetchall():
-            results['Accession'] = str(set(row[0]))
-            results['GeneID'] = str(set(row[1]))
-            results['Protein'] = str(set(row[2]))
-            results['Locus'] = str(set(row[3]))
-            results['Date'] = str(set(row[4]))
-            results['Description'] = str(set(row[5]))
-            results['Source'] = str(set(row[6]))
-            results['Sequence'] = str(set(row[7]))
-            results['Coding_seq'] = str(set(row[8]))
-            results['Frame'] = int(set(row[9]))
-            results['Translation'] = str(set(row[10]))
-            results['Coding Regions']['{}'.format(str(row[11]))] = str(row[12])
+            results['Accession'] = row[0]
+            results['Locus'] = row[2]
+            results['GeneID'] = row[3]
+            results['Product'] = row[4]
+            results['Description'] = row[5]
+            results['Source'] = row[6]
+            results['Sequence'] = row[7]
+            results['Frame'] = row[8]
+            results['Translation'] = row[9]
+            results['Coding Regions'][row[11]] = row[12]
     
     cursor.close()
-    connection.close()
     
     return results
 
 def getAllEntries():
     '''
     Returns a list of dictionaries to display on the search page
-    keys(): 'Accession', 'GeneID', 'Protein', 'Locus'
+    keys(): 'Accession', 'GeneID', 'Product', 'Locus'
     '''
     try:
         return search('getAll', 'getAll', 0)
@@ -120,7 +119,7 @@ def getByAccession(query: str, resultslen: int):
         if len(results) == 0:
             return 'No results found'
         if len(results) == 1:
-            return getAccession(results['Accession'])
+            return getAccession(results[0]['Accession'])
         else:
             return results
     except pymysql.err.Error as e:
@@ -137,7 +136,7 @@ def getByGeneID(query: str, resultslen: int):
         if len(results) == 0:
             return 'No results found'
         if len(results) == 1:
-            return getAccession(results['Accession'])
+            return getAccession(results[0]['Accession'])
         else:
             return results
     except pymysql.error.Error as e:
@@ -153,23 +152,23 @@ def getByLocus(query: str, resultslen: int):
         if len(results) == 0:
             return 'No results found'
         if len(results) == 1:
-            return getAccession(results['Accession'])
+            return getAccession(results[0]['Accession'])
         else:
             return results
     except pymysql.err.Error as e:
         return 'Database error {}'.format(e)
 
-def getByProtein(query: str, resultslen: int):
+def getByProduct(query: str, resultslen: int):
     '''
     Function that will return list of summary information on entries whose proteins descriptions are similar to the query.
     If query returns unique entry, calls getAccession(result's accession) to return details for gene page.
     '''
     try:
-        results = search('Protein', query, resultslen)
+        results = search('Product', query, resultslen)
         if len(results) == 0:
             return 'No results found'
         if len(results) == 1:
-            return getAccession(results['Accession'])
+            return getAccession(results[0]['Accession'])
         else:
             return results
     except pymysql.err.Error as e:
--- createdb/createdb.py
+++ createdb/createdb.py
@@ -90,11 +90,11 @@ import pymysql
 from pymysql import cursors
 
 #Database connection details:
-dbname = 'biodb'
+dbname = 'localhost'
 dbhost = 'pandora'
 port = 3306
-dbuser = 'biodb_user'
-dbpass = 'biodb_p'
+dbuser = 'cw001'
+dbpass = 'trp38ile'
 
 #Connection object
 connection = pymysql.connect(
@@ -109,61 +109,51 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 #Create tables
-create_tbls = (
-'DROP TABLE IF EXISTS coding_regions;'
-'DROP TABLE IF EXISTS genes;'
+create_tbls = ('''
+DROP TABLE IF EXISTS coding_regions;
+DROP TABLE IF EXISTS genes;
 
-'CREATE TABLE genes('
-    'Accession VARCHAR(12) PRIMARY KEY,'
-    'Date DATE NOT NULL,'
-    'Locus VARCHAR(40) NOT NULL,'
-    'GeneID VARCHAR(8) NOT NULL,'
-    'Protein VARCHAR(255) NOT NULL,'
-    'Description VARCHAR(255) NOT NULL,'
-    'Source VARCHAR(60) NOT NULL,'
-    'Sequence LONGBLOB NOT NULL,'
-    'Coding_Seq LONGBLOB NOT NULL,'
-    'Frame INT(1) NOT NULL,'
-    'Translation LONGBLOB NOT NULL' 
-')ENGINE = InnoDB;'
+CREATE TABLE genes(
+    Accession VARCHAR(12) PRIMARY KEY,
+    Date DATE NOT NULL,
+    Locus VARCHAR(40) NOT NULL,
+    GeneID VARCHAR(8) NOT NULL,
+    Product VARCHAR(255) NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    Source VARCHAR(60) NOT NULL,
+    Sequence LONGBLOB NOT NULL,
+    Frame INT(1) NOT NULL,
+    Translation LONGBLOB NOT NULL 
+)ENGINE = InnoDB;
 
-'CREATE TABLE coding_regions('
-    'Accession VARCHAR(12) PRIMARY KEY,'
-    'Region_No INT(100) NOT NULL,'
-    'Base_No VARCHAR(24) NOT NULL,'
-    'FOREIGN KEY(Accession)'
-        'REFERENCES genes(Accession)'
-        'ON DELETE CASCADE'
-')Engine = InnoDB;'
-)
+CREATE TABLE coding_regions(
+    Accession VARCHAR(12) NOT NULL,
+    Region_No VARCHAR(8) NOT NULL,
+    Bases VARCHAR(24) NOT NULL,
+    FOREIGN KEY(Accession)
+        REFERENCES genes(Accession)
+        ON DELETE CASCADE
+)Engine = InnoDB;
+''')
 
 try:
     cursor.execute(create_tbls)
     print('Successfully created tables')
-    cursor.commit()
 except pymysql.err.Error as e:
     print(f'Failed creating tables: {e}')
     exit(1)
     
 #Load data into SQL Server
-load_genetbl = (
-'INSERT INTO genes(Accession, Date, Locus, GeneID, Protein, Description, Source, Sequence, Frame, Translation)'
-    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
-)
-
-load_codingtbl = (
-'INSERT INTO coding_regions VALUES(%s, %s, %s);'
-)
-
-cursor.execute(load_genetbl, (accessions, dates, loci, geneIDs, protein_products, descriptions, sources, sequences, reading_frames, translations))
-cursor.commit()
+cursor = connection.cursor()
+for i in range(0, 861):
+    cursor.execute('INSERT INTO genes VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                  (accessions[i], dates[i], loci[i], geneIDs[i], protein_products[i], descriptions[i], sources[i], sequences[i], reading_frames[i], translations[i]))
 
 i = -1 #Counter to reference accession numbers for DB Table PK
 for entry in coding_regions:
     i += 1
     for k, v in entry.items():
-        cursor.execute(load_codingtbl, (accessions[i], k, v)) #Loads coding regions (keys) and base ranges (values) into table with accession number for PK
-        cursor.commit()
-
+        cursor.execute('INSERT INTO coding_regions VALUES (%s, %s, %s)', (accessions[i], k, v)) #Loads coding regions (keys) and base ranges (values) into table with accession number for PK
+        
 cursor.close()
 connection.close()
