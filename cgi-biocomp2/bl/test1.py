cec3c43e7df1f23f8afe5f55b398a973cf7f112c
tiinatalts <tiina.talts@phe.gov.uk>
2022-03-26 19:23:22 +0000

    testing test1.py

---
+++ cgi-biocomp2/bl/test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+TT
+'''
+
+
+import sys
+sys.path.insert(0, "../db")
+sys.path.insert(0, "../")
+
+
+import dbapi as db
+import config
+
+new_file = "../bl/AB032150_getAccession.txt"
+
+with open(new_file, 'w') as f:
+	tt = db.getAccession('AB032150')
+	f.write(tt)
