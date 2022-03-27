bb33d52d741c089fed83ceefcbd3d5400af9a0b7
tiinatalts <tiina.talts@phe.gov.uk>
2022-03-27 16:48:03 +0100

    testing test1.py

--- cgi-biocomp2/bl/test1.py
+++ cgi-biocomp2/bl/test1.py
@@ -12,8 +12,9 @@ sys.path.insert(0, "../")
 import dbapi as db
 import config
 
-new_file = "../bl/AB032150_getAccession.txt"
+#new_file = "../bl/AB032150_getAccession.txt"
 
-with open(new_file, 'w') as f:
-	tt = db.getAccession('AB032150')
-	f.write(str(tt))
+#with open(new_file, 'w') as f:
+tt = db.getAccession('AB032150')
+	#f.write(str(tt))
+print(tt)
