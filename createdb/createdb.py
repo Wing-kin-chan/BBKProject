0831cba1df4a320874c8e84d6a48ecbe94a1d87d
Wing-kin Chan <wing_kin_chan@hotmail.com>
2022-03-28 09:12:40 +0100

    +Coding_seq DB column

--- createdb/createdb.py
+++ createdb/createdb.py
@@ -110,7 +110,7 @@ cursor = connection.cursor()
 create_db = dict()
 create_db['drop coding table'] = 'DROP TABLE IF EXISTS coding_regions;'
 create_db['drop gene table'] = 'DROP TABLE IF EXISTS genes;'
-create_db['gene_tbl'] = 'CREATE TABLE genes(Accession VARCHAR(12) PRIMARY KEY, Date DATE NOT NULL, Locus VARCHAR(40) NOT NULL, GeneID VARCHAR(8) NOT NULL, Product VARCHAR(255) NOT NULL, Description VARCHAR(255) NOT NULL, Source VARCHAR(60) NOT NULL, Sequence LONGBLOB NOT NULL, Frame INT(1) NOT NULL, Translation LONGBLOB NOT NULL, CD_seq LONGBLOB);'
+create_db['gene_tbl'] = 'CREATE TABLE genes(Accession VARCHAR(12) PRIMARY KEY, Date DATE NOT NULL, Locus VARCHAR(40) NOT NULL, GeneID VARCHAR(8) NOT NULL, Product VARCHAR(255) NOT NULL, Description VARCHAR(255) NOT NULL, Source VARCHAR(60) NOT NULL, Sequence LONGBLOB NOT NULL, Frame INT(1) NOT NULL, Translation LONGBLOB NOT NULL, Coding_seq LONGBLOB);'
 create_db['coding_tbl'] = 'CREATE TABLE coding_regions(Accession VARCHAR(12), Region_No VARCHAR(8), Bases VARCHAR(24), FOREIGN KEY(Accession) REFERENCES genes(Accession) ON DELETE CASCADE);'
 
 for k, v in create_db.items():
@@ -128,7 +128,7 @@ cursor.close()
 #Load data into SQL Server
 cursor = connection.cursor()
 for i in range(0, 861):
-    cursor.execute('INSERT INTO genes VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+    cursor.execute('INSERT INTO genes VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) SET Coding_seq NULL',
                   (accessions[i], dates[i], loci[i], geneIDs[i], protein_products[i], descriptions[i], sources[i], sequences[i], reading_frames[i], translations[i]))
 
 i = -1 #Counter to reference accession numbers for DB Table PK
