a1337afcf24853b2b9a1b28c1ae18d8c5c6e0180
Wing-kin Chan <wing_kin_chan@hotmail.com>
2022-04-05 14:15:03 +0100

    Added feature class

--- createdb/parser.py
+++ createdb/parser.py
@@ -5,10 +5,6 @@ Code for parsing GenBank files.
 '''
 import re
 
-GB_FIRST_LINE = 79
-GB_ANNOT_INDENT = 12
-GB_FEATR_INDENT = 21
-
 class Record:
     '''
     Holds all information of a GenBank record
@@ -36,17 +32,24 @@ class Record:
       Within each main header, there will be a location type [0-9]..[0-9] or a qualifier type /XXXXXX="" The type 
       will be stored as a type variable in each feature object.
       For qualifiers, the string will be stored as a key variable within each feature object.
-        {source: [(1:268, type = 'location'),
-                 ('Homo sapiens, type = 'qualifier', key = 'organism'),
-                 ('10', type = 'qualifier', key = 'chromosome'),
-                 ('10q23', type = 'qualifier', key = 'map']), etc...
-         CDS:    [('1:30', '50:94', '121:174', '203:259', type = 'location'),
-                 ('bifunctional enzyme', type = 'qualifier', key = 'function'),
-                 ('fructose-6-phosphate kinase', type = 'qualifier', key = 'product'),
-                 ('MATRGNPILLLEYAAPLREVLCCYAL', type = 'qualifier', key = 'translation') etc...]
+        {source: [(value = '1:268', type = 'location'),
+                 (value = 'Homo sapiens', type = 'qualifier', key = 'organism'),
+                 (value = '10', type = 'qualifier', key = 'chromosome'),
+                 (value = '10q23', type = 'qualifier', key = 'map']), etc...
+         CDS:    [(value = '1:30', '50:94', '121:174', '203:259', type = 'location'),
+                 (value = 'bifunctional enzyme', type = 'qualifier', key = 'function'),
+                 (value = 'fructose-6-phosphate kinase', type = 'qualifier', key = 'product'),
+                 (value = 'MATRGNPILLLEYAAPLREVLCCYAL', type = 'qualifier', key = 'translation') etc...]
         }
     - sequence - A string to store the sequence provided under the ORIGIN header of GenBank files.
     '''
+    
+    GB_MAX_LINE_LEN = 79
+    GB_ANNOT_INDENT = 12
+    GB_FEATR_INDENT = 21
+    GB_SEQ_LINE_LEN = 75
+    GB_SEQ_START = 10
+    GB_SEQ_END = 74
     
     def __init__(self):
         '''Initialise the Record class'''
@@ -60,3 +63,23 @@ class Record:
         self.references = dict()
         self.features = dict()
         self.sequence = str()
+
+class Feature:
+    '''
+    Class to hold information on GenBank features
+    
+    Attributes:
+    - type - Location or Qualifier
+    - key - A key that references the qualifier title as is in the GenBank record e.g 'translation'.
+            If type is location, key is None
+    - value - The value of the feature e.g 'MAHGLLIEPA...'
+    '''
+    GB_LOCATION = re.compile(r'(>|<)*[0-9]+\.{2}[0-9]+')
+    GB_QUALIFIER = re.compile(r'\B/[a-zA-Z]+\_*[a-zA-Z]+')
+    
+    def __init__(self, type = str, key = str, value = str, ):
+        '''Initialise the features class'''
+        self.type = str()
+        self.key = str()
+        self.value = str()
+        
