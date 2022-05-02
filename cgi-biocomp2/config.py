cd7a7148a3ed253a8c3ad7f5052a73cc2d89498c
Wing-kin-chan <wing_kin_chan@hotmail.com>
2022-05-02 17:49:15 +0100

    .gitignore updates

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """
-Database connection and URLs for cgi scripts and website navigation
+Database connection, and URLs for cgi scripts and website navigation
 """
 
-###Config Info###
+#URLs:
+cgiURL = "/cgi-bin/cgiwrap/cw001"
+searchurl = cgiURL + "/biocomp2demo/cgi/search.cgi"
+listallurl = cgiURL + "/biocomp2demo/cgi/listall.cgi"
+
+#Database Connection
+dbname = "cw001"
+dbhost = "localhost"
+port = 3306
+dbuser = "cw001"
+dbpass = "trp38ile"
